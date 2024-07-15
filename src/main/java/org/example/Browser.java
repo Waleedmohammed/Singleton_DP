e652efe180763ca115709411c695ce8f16543fc1
waleed.elbarbary <n448321@pagonxt.com>
2024-07-15 10:45:52 +0200

    Adding Singleton implementation

---
+++ src/main/java/org/example/Browser.java
@@ -0,0 +1,31 @@
+package org.example;
+
+public class Browser {
+
+    // 1. Private static instance of the class
+    private   static Browser browser;
+
+    // 2. private constructor of the class
+    private Browser() {
+
+    }
+
+    // 3. public static getInstance method
+    public static Browser getBrowser() {
+        if (browser == null) {
+            synchronized (Browser.class){
+                if (browser == null) {
+                    browser = new Browser();
+                }
+            }
+        }
+        return browser;
+    }
+
+    // 4. Individual public method
+    public void displayMessage(){
+        System.out.println("Hi Singleton");
+    }
+
+
+}
